b772925d82f3b1a6cb71edcaf64132b8ad5151c1
mmcmille <mcmillen.michael.s@gmail.com>
2022-02-13 14:12:38 -0800

    Update Run_Caster_Kaldi.bat

--- Run_Caster_Kaldi.bat
+++ Run_Caster_Kaldi.bat
@@ -4,6 +4,6 @@ echo Running Kaldi from Dragonfly CLI
 set currentpath=%~dp0
 
 TITLE Caster: Status Window
-python -m dragonfly load _*.py --engine kaldi --no-recobs-messages --engine-options "model_dir=kaldi_model, vad_padding_end_ms=300"
+python -m dragonfly load _*.py --engine kaldi --engine-options "model_dir=C:\Users\mandm\Documents\Caster-kaldi\kaldi_model_small, audio_self_threaded=False, audio_auto_reconnect=True, vad_padding_start_ms=150, vad_padding_end_ms=200, vad_complex_padding_end_ms=600, invalidate_cache=True, auto_add_to_user_lexicon=True, allow_online_pronunciations=True, lazy_compilation=False, vad_aggressiveness=2, expected_error_rate_threshold=0.1"
 
 pause 1
