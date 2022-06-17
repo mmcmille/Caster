cfb0975c1132c4b6522ae8226c1ddc0aedf670b4
mmcmille <mcmillen.michael.s@gmail.com>
2022-06-17 12:34:16 -0700

    up

--- castervoice/rules/core/navigation_rules/home_window_mgmt_rule.py
+++ castervoice/rules/core/navigation_rules/home_window_mgmt_rule.py
@@ -1,5 +1,4 @@
-from dragonfly import MappingRule, Function, Repeat, Pause, Choice
-
+from dragonfly import Dictation, MappingRule, Function, Repeat, Pause, Choice, BringApp, ShortIntegerRef, RunCommand
 from castervoice.lib import utilities
 from castervoice.lib import virtual_desktops
 from castervoice.lib.actions import Key, Text, Mouse
@@ -10,24 +9,61 @@ from castervoice.lib.merge.state.short import R
 
 class HomeWindowManagementRule(MappingRule):
     mapping = {
+        #app switching via control clicks area on vertical taskbar, 1-10
+        "[<close_choice>] (show|open|window) <app_n_key>":
+            R(
+                Key("cw-%(app_n_key)s") +
+                Pause("100") +
+                Key("%(close_choice)s")
+            ),
 
-        #app switching via control clicks area on vertical taskbar
-        "(show|open|switch|window) <app_n>":
-            R(Key("control:down") +
-              Mouse("".join(["[30,","%(app_n)s", "], left"])) +
-              Key("control:up/20") +
-              Pause("200")+
-              Mouse("(0.5, 0.5)")
-              ),
+        "[<close_choice>] (show|open|window) <app_n_11>": #<app_n>":
+            R(
+                Key("w-t/20, down:9") +
+                Key("down:%(app_n_11)s, enter") +
+                #Key("control:down") +
+              #Mouse("".join(["[30,","%(app_n)s", "], left"])) +
+              #Key("control:up/20") +
+                Pause("100")+
+                Key("%(close_choice)s")),
 
-
+        #app switching via fluent search
+        "switch [<text>]": R(Key("csa-p") + Pause( "100") + Text("%(text)s ")),
+        #"taskbar <n>": R(Key("w-%(n)s")),
         "track mouse": R(Key("f9", use_hardware=True)),
-
-
-
     }
 
     extras = [
+        ShortIntegerRef("n_1_9", 1, 9),
+        Dictation("text"),
+        Choice("close_choice",{
+            "close":"a-f4",
+            "":"",
+        }),
+        Choice("app_n_key", {
+            "(1|web)": 1,
+            "(2|files)": 2,
+            "(3|commands)": 3,
+            "(4|notes)": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+            "10": 0,
+        }),
+        Choice("app_n_11", {
+            "11": 1,
+            "12":  2,
+            "13":  3,
+            "14":  4,
+            "15":  5,
+            "16":  6,
+            "17":  7,
+            "18":  8,
+            "19":  9,
+            "20":  10
+        }),
         Choice("app_n", {
          	"(1|web)": 64,
             "(2|files)": 111,#+47@100%
